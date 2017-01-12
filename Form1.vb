04d58ba24e57fb8dae894674b5036e55f20a4cd9
karankashyap@bitbucket.com <karankashyap@live.com>
2017-01-13 00:07:54 +0530

    First commit

---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub addClick(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Database1DataSet1.data' table. You can move, or remove it, as needed.
+        Me.DataTableAdapter1.Fill(Me.Database1DataSet1.data)
+        'TODO: This line of code loads data into the 'Database1DataSet.data' table. You can move, or remove it, as needed.
+        Me.DataTableAdapter.Fill(Me.Database1DataSet.data)
+
+        ' Set the caption bar text of the form.   
+        Me.Text = "tutorialspont.com"
+    End Sub
+    Private Function CreateDataSet() As DataSet
+        'creating a DataSet object for tables
+        Dim dataset As DataSet = New DataSet()
+        ' creating the student table
+        Dim Students As DataTable = CreateStudentTable()
+        dataset.Tables.Add(Students)
+        Return dataset
+    End Function
+    Private Function CreateStudentTable() As DataTable
+        Dim Students As DataTable
+        Students = New DataTable("Student")
+        ' adding columns
+        AddNewColumn(Students, "System.Int32", "StudentID")
+        AddNewColumn(Students, "System.String", "StudentName")
+        AddNewColumn(Students, "System.String", "StudentCity")
+        ' adding rows
+        AddNewRow(Students, 1, "Zara Ali", "Kolkata")
+        AddNewRow(Students, 2, "Shreya Sharma", "Delhi")
+        AddNewRow(Students, 3, "Rini Mukherjee", "Hyderabad")
+        AddNewRow(Students, 4, "Sunil Dubey", "Bikaner")
+        AddNewRow(Students, 5, "Rajat Mishra", "Patna")
+        Return Students
+    End Function
+    Private Sub AddNewColumn(ByRef table As DataTable,
+   ByVal columnType As String, ByVal columnName As String)
+        Dim column As DataColumn =
+       table.Columns.Add(columnName, Type.GetType(columnType))
+    End Sub
+
+    'adding data into the table
+    Private Sub AddNewRow(ByRef table As DataTable, ByRef id As Integer, ByRef name As String, ByRef city As String)
+        Dim newrow As DataRow = table.NewRow()
+        newrow("StudentID") = id
+        newrow("StudentName") = name
+        newrow("StudentCity") = city
+        table.Rows.Add(newrow)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ds As New DataSet
+        ds = CreateDataSet()
+        DataGridView2.DataSource = ds.Tables("Student")
+
+
+
+    End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentClick
+
+    End Sub
+End Class
---
+++ Form3.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form3
+    Public FI As Object
+    Public Qry As String
+    Public rst As New ADODB.Connection
+    Public daoEngine As DAO.DBEngine
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FI = CreateObject("Busy2L16.CFixedInterface")
+        Console.WriteLine(FI)
+
+        FI.OpenDB("D:\Program Files (x86)\BusyWin", "D:\Program Files (x86)\BusyWin\DATA\", "Comp0002")
+
+        MsgBox("Database Connected")
+        Qry = "Select * from Tran1 where VchType = 9"
+        'This query will return list of all the Sale vouchers as VchType=9 belong to SALE voucher in Tran1 table
+
+        Qry = "Select * from Master1 where MasterType=6"
+        'This query will return list of all the Item Masters as MasterType=6 belongs to Item Master in Master1 table.
+
+        Debug.Print(FI)
+        rst = FI.GetAllVchSeriesCodes(3)
+
+        'rst = FI.OpenRecordset(Qry)
+
+        Debug.Print(rst)
+        If rst.RecordCount > 0 Then
+            rst.MoveFirst
+            rst.MoveLast
+            Do While Not rst.EOF
+                MsgBox(Trim$(rst!Name.Value))
+                rst.MoveNext
+            Loop
+        End If
+        FI.CloseDB
+
+    End Sub
+End Class
